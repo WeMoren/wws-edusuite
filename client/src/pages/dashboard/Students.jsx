488c208487fbceb5c202b706a06916c09c121f85
Uwem <wemoren@gmail.com>
2026-08-07 12:55:05 +0100

    Added students page header and action button

--- client/src/pages/dashboard/Students.jsx
+++ client/src/pages/dashboard/Students.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import StudentTable from '../../components/students/StudentTable'
+import "./Students.css"
 const Students = () => {
   return (
     <div className='student-page'>
-        <h1>Students</h1>
+        <div className="students-page__header">
+             <h1>Students</h1>
+
+             <button className="students-page__button">
+                + Add Student
+             </button>
+        </div>
 
         <StudentTable/>
     </div>
