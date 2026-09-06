fec7d9a81e098803f90637e7c8275494b6b90d6b
Uwem <wemoren@gmail.com>
2026-09-06 21:51:24 +0100

    Connectd backend to PostgreSQL

--- server/src/server.js
+++ server/src/server.js
@@ -2,6 +2,8 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import express from "express";
+import pool from "./config/db.js";
+
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -12,6 +14,15 @@ app.get("/", (req, res) => {
     });
 });
 
+pool.query("SELECT NOW()")
+    .then(() => {
+        console.log("PostgreSQL database connected");
+    })
+    .catch((error) => {
+        console.error("PostgreSQL connection failed:", error.message);
+    });
+
 app.listen(PORT, ()  => {
     console.log(`WWS-Edusuite is running on port ${PORT}`)
 })
+
